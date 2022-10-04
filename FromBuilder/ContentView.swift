92968480329e93782d95d21ba70c836b89100b61
Pouya Yarandi <p.yarandi@gmail.com>
2022-10-04 21:53:53 +0330

    Initial Commit

---
+++ FromBuilder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FromBuilder
+//
+//  Created by Pouya on 7/12/1401 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FromBuilder/FromBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FromBuilderApp.swift
+//  FromBuilder
+//
+//  Created by Pouya on 7/12/1401 AP.
+//
+
+import SwiftUI
+
+@main
+struct FromBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
